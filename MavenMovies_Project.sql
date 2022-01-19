4c467f3351405298dc2f82fd180c830a8137ef79
Damilare Faluyi <92920276+damilareGit@users.noreply.github.com>
2022-01-19 03:42:55 +0100

    Update MavenMovies_Project.sql

--- MavenMovies_Project.sql
+++ MavenMovies_Project.sql
@@ -1,4 +1,4 @@
-# MySql Project on using MavenMovies database.
+# MySql Project using MavenMovies database.
 
 /*
 1.	We will need a list of all staff members, including their first and last names, 
